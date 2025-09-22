12a43b8a248fb36027b0e03285eb39dd76cfb967
Astre06 <122554381+Astre06@users.noreply.github.com>
2025-09-22 22:09:02 +0800

    Update mass_check.py

--- mass_check.py
+++ mass_check.py
@@ -8,7 +8,7 @@ import proxy
 from telegram import InputFile, InlineKeyboardButton, InlineKeyboardMarkup
 from bininfo import round_robin_bin_lookup
 from auth_processor import generate_uuids, prepare_headers, check_card_across_sites
-from config import TELEGRAM_BOT_TOKEN, DEFAULT_API_URL, MAX_WORKERS
+from config import TELEGRAM_BOT_TOKENS, DEFAULT_API_URL, MAX_WORKERS
 from manual_check import check_ip_via_proxy_async, get_own_ip_async
 
 SITE_STORAGE_FILE = "current_site.txt"
@@ -280,5 +280,6 @@ async def handle_file(update, context):
                     print(f"Deleted temporary file: {file_path}")
             except Exception as e:
                 print(f"Failed to delete {file_path}: {e}")
+
 
 
